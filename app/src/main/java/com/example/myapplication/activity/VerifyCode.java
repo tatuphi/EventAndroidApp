74b7dd0691ee80d3b852f32510645c632dc21305
tatuphi <tatuphi@gmail.com>
2020-05-24 00:53:07 +0700

    forget pass & verify code

---
+++ app/src/main/java/com/example/myapplication/activity/VerifyCode.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.myapplication.R;
+
+public class VerifyCode extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_code);
+    }
+}
